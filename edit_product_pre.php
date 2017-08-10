570d82d2d59f131aed69d740586c71d628a5825c
B <bcm811@gmail.com>
2017-08-10 17:03:40 -0600

    found a workaround involving jquery and serializing the form, srtupid anti-XSS security measure prevent putting code into an html element now

--- edit_product_pre.php
+++ edit_product_pre.php
@@ -1,10 +1,29 @@
 <?php
+/**
+*
+*	This entire editor-workaround was created because the behavior of Proline's Zencart
+*   Installation has become erratic. A broswer error in multiple different browsers (ie: 
+*   Chrome, Firefox, Safari) BROSWER ERROR STILL UNKNOWN 
+*	Somewhere in zencart's product editor/creator functionality the problems lie
+*	1. The browser thinks some element in the editor page is insecure, and cuts the database
+*		query while zencart still thinks it's updating the product, this then causes it to
+*      wipe whatever database entry was already there to nothing
+*	2. For some reason, we do not have the ability to create new products with zencart's
+*	   Current functionality, so I am creating both editors and new product creators outside
+*	   of using zencarts functions in order to have something that I know will work since 
+*      it's directly manipulating the database through my own commands
+*    UPDATE: Here is chrome's error reasoning ERR_BLOCKED_BY_XSS_AUDITOR
+*
+*/
+
+// Sending header reference maybe this will fix the chrome bug??
+header('X-XSS-Protection:0');
 
 $productId = '311';
-
+$productId = (int) $productId;
 $db_name = 'proliner_zcart';
 		$db_user = 'root';
-		$db_pass = 'proline55';
+		$db_pass = '***';
 		$host = 'localhost';
 		$charset = 'utf8';
 		$table = 'products_description';	
@@ -18,7 +37,6 @@ $db_name = 'proliner_zcart';
 $products_description = $stuff['products_description'];
 $products_id = $stuff['products_id'];
 $products_name = $stuff['products_name'];
-var_dump($products_name);
 
 
 $product = array(
@@ -33,7 +51,7 @@ $product = array(
 
 $db_name = 'proliner_zcart';
 		$db_user = 'root';
-		$db_pass = 'proline55';
+		$db_pass = '***';
 		$host = 'localhost';
 		$charset = 'utf8';
 		$table = 'products_description';	
@@ -42,14 +60,27 @@ $db_name = 'proliner_zcart';
 		$result = $conn->query($sql);
 		$stuff = $result->fetch_array(MYSQLI_ASSOC);
 
+$products_id = $productId;
 $products_quantity = $stuff['products_quantity'];
 $products_price = $stuff['products_price'];
 $products_status = $stuff['products_status'];
+$products_model = $stuff['products_model'];
+$products_type = $stuff['products_type'];
+
+/*
+* I don't know why I originally started with defining these variables before hand
+* then setting them to the correct array['key'] afterwards to prepare them for the view
+* -B
+*/
+$product['id'] = $products_id;
+$product['products_model'] = $products_model;
 $product['products_status'] = $products_status;
 $product['products_quantity'] = $products_quantity;
 $product['products_price'] = $products_price;
+// Apparently I stopped here, much less typing ;-)
 $product['products_price'] = $stuff['products_price'];
 $product['products_model'] = $stuff['products_model'];
 $product['products_quantity'] = $stuff['products_quantity'];
+$product['products_image'] = $stuff['products_image'];
 require_once('new_edit_product.php');
 
--- new_edit_product.php
+++ new_edit_product.php
@@ -1,8 +1,11 @@
 <head>
 <style>
+*
+{
+	margin-top:20px;
+}
 .np-container
 {
-	padding:100px;
 	width:100%;
 }
 .np-form-holder
@@ -16,34 +19,60 @@
 	width:100%;
 	height:25px;
 }
+#np-product-thumb
+{
+	max-height:50px;
+	max-width:50px;
+}
 </style>
 </head>
 
-<?php var_dump($product['products_status']);?>
-<?php echo $product['products_id'];?>
+
+
 <div class='np-container'>
 	<div class='np-form-holder'>
-		<form name='edit-product'>
+		<h4>Editing Product ID#: <?php echo $product['id'];?></h4>
+		<img id='np-product-thumb' src="<?php echo  '/images/' . $product['products_image'];?>"/>
+		<form id='np-form' name='edit-product' method='POST' action='edit_product_action.php'>
 		<div class='np-spacer'></div>
+		<?php Header('X-XSS-Protection: 0');?>
 			<label>Product Name</label><br>
 			<textarea style='width:100%;' name='products_name'><?php echo $product['products_name'];?></textarea>
 			<div class='np-spacer'></div>
-			<label>Quantity</label>
-			<input type='number' value="<?php echo $product['products_quantity'];?>"/><br>
+
+			<label>Model</label><br>
+			<input type='text' name='products_model' value="<?php echo $product['products_model'];?>"/>
 			<div class='np-spacer'></div>
+
+			<label>Quantity</label><br>
+			<input type='number' name='products_quantity' value="<?php echo $product['products_quantity'];?>"/><br>
+			<div class='np-spacer'></div>
+
 			<label>Turn product on/off</label><br>
 			<label>On</label>
-			<input type='radio' value='status' <?php echo ($product['products_status'] === '1' ? "checked='checked'" : null); ?> name='on'><br>
+			<input type='radio' value='1' name='products_status' <?php echo ($product['products_status'] === '1' ? "checked='checked'" : null); ?> name='on'><br>
 			<label>Off</label>
-			<input type='radio' value='status' name='off' <?php echo ($product['products_status'] === '0' ? "checked='checked'" : null); ?>>
-			<br>
-			
+			<input type='radio' value='0' name='products_status' <?php echo ($product['products_status'] === '0' ? "checked='checked'" : null); ?>><br>
 			<div class='np-spacer'></div>
+
 			<label>Gross Price</label><br>
-			<p><?php echo $product['products_price'];?>   <span style='color:red; font-size:8px;'>Edit Price in other area</span></p>
+			<p><?php echo $product['products_price'];?><span style='color:red; font-size:8px;'>Edit Price in other area</span></p>
 			<label>DESCRIPTION</label><br>
-			<textarea style='width:600px; height:400px;' value="<?php echo $product['products_description'];?>"/><br>
+			<textarea type='text' name='products_description' style='width:600px; height:400px;' value="<?php echo $product['products_description'];?>"></textarea><br>
+			<div class='np-spacer'></div>
+			<button type='button' id='np-submit-button'>Submit</button>
 		</form>
 	</div>
 </div>
+<script src="https://code.jquery.com/jquery-latest.js"></script>
+<script>
+$(function(){
+	var button = $('#np-submit-button');
+	var form = $('#np-form');
+	button.on('click', function(){
+		var stuff = form.serializeArray();
+		console.log(stuff);
+	})
+})
 
+</script>

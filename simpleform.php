df24d2f6ea1656f15e3c16764ed662131dec9a79
Brian Moniz <brian@prohoods.com>
2017-08-15 13:52:29 -0600

    added simpleform to add to /adminpro/categories.php file

---
+++ simpleform.php
@@ -0,0 +1,7 @@
+<tr>
+          <h3 style='color:red;'>USE THIS FOR PRODUCT EDITING</h3>
+            <form name='edit-product' method='POST' action='editor-workaround/edit_product_pre.php'>
+            <input type='number' name='productId' placeholder='productID'/>
+            <input type='submit'>
+            </form>
+          </tr>
